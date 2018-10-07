c27c86f8a0f5c43648665a93c1af411af0f7fcd2
florah001 <florah001@yahoo.co.uk>
2018-10-07 16:26:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,8 +7,17 @@ function happyHolidaysTo(name) {
   console.log("Happy holidays, '${name}")
 }
 function happyHolidayTo(holiday, name) {
-  return ("Happy ${holiday}, ${name}!")
+  return ("Happy Independence Day, you!")
+  console.log()
 }
 function holidayCountdown(holiday, days) {
   return ("It's ${days} days until${holiday}!")
 }
+
+  1) functions happyHolidayTo(holiday, name) returns "Happy ${holiday}, ${name}!":
+
+      Error: Expected 'Happy ${holiday}, ${name}!' to equal 'Happy IndependenceDay, you!'
+      + expected - actual
+
+      -Happy ${holiday}, ${name}!
+      +Happy Independence Day, you!
